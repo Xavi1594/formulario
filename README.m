5e837212625586ee015d276645bd43f2739c5138
Xavi Villaverde <115170876+Xavi1594@users.noreply.github.com>
2022-12-05 08:30:51 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,19 @@
+# Proyecto FORMULARIO HTML CSS
+## PROYECTO
+<a href="http://johnnyramirez.es/f5/FORMULARIO-HTML-CSS/prototipo.jpg" target="_blank"><img src="http://johnnyramirez.es/f5/FORMULARIO-HTML-CSS/prototipo.jpg" alt="Prototipo" width="200"></a>
+<a href="http://johnnyramirez.es/f5/FORMULARIO-HTML-CSS/index.jpg" target="_blank"><img src="http://johnnyramirez.es/f5/FORMULARIO-HTML-CSS/index.jpg" alt="Index" width="200"></a>   
+A partir de un prototipo presentado, se nos solicita reproducir un formulario y añadirle la función de mostrar los datos recogidos en la consola de javascript.
+## PARTES DEL PROYECTO
+### HTML
+Creamos la estructura necesaria para llevar a cabo el proyecto, atendiendo a la diversidad de campos de formulario necesarios.
+### CSS
+A través de CSS, y siguiendo el prototipo presentado, se adapta la visualización del proyecto a dicho protitipo.
+## HERRAMIENTAS Y LENGUAJES UTILIZADOS
+### Lenguajes
+- HTML
+- CSS
+### Herramientas
+- Visual Studio Code, junto a Git Bash, para la creación de los elementos necesarios (documentos HTML, CSS , así como las carpetas necesarias para su organización) y el repositorio local.
+- GitHub para la creación de un repositorio online que nos permitirá, entre otras cosas, recuperar el proyecto desde cualquier dispositivo, así como compartirlo llegado el momento.
+## AUTOR
+- @Xavi1594
